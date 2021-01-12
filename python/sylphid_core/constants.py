6427e1898e49df21b6fdb6c916c9cff392042cf9
rfletchr <rcsfletcher@protonmail.com>
2021-01-12 08:56:04 +0000

    add comment

--- python/sylphid_core/constants.py
+++ python/sylphid_core/constants.py
@@ -2,4 +2,6 @@
 CTX_ENV_KEY_ORDER = ("CTX_PRJ", "CTX_SEQ", "CTX_SHT", "CTX_TSK")
 CTX_DICT_KEY_ORDER = ("project", "sequence", "shot", "task")
 
-PATH_REFERENCE_REGEX = r"\<([A-Za-z0-9]+)\>"
+# regex to template references in unresolved template strings.
+# eg. <root>, <project_name>, <ProjectName>
+PATH_REFERENCE_REGEX = r"\<([A-Za-z0-9_]+)\>"
